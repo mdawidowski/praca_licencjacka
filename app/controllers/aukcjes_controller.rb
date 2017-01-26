8b869fdde23d359fa6a7027b5e82ef35dd1ba9e6
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2017-01-26 22:27:21 +0100

    poprawiono dodawanie aukcji

--- app/controllers/aukcjes_controller.rb
+++ app/controllers/aukcjes_controller.rb
@@ -4,14 +4,31 @@ class AukcjesController < ApplicationController
   end
 
   def pokaz
+    @aukcje = Aukcje.find(params[:id])
   end
 
-  def nowa
+  def new
+    @aukcje = Aukcje.new
   end
 
-  def edycja
+  def show
   end
 
-  def usun
-  end
+  def create
+      @aukcje = Aukcje.new(aukcje_params)
+
+      respond_to do |format|
+        if @aukcje.save
+          format.html { redirect_to @aukcje, notice: 'Aukcja pomyślnie dodana.' }
+          format.json { render :show, status: :created, location: @aukcje }
+        else
+          format.html { render :new }
+          format.json { render json: @aukcje.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def aukcje_params
+        params.require(:aukcje).permit(:nazwa, :opis, :data_zakonczenia, :cena_startowa, :cena_minimalna, :koszt_przesylki)
+      end
 end
