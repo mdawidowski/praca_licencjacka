362d252f7152b9512d42b352103dbd7416806545
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2017-02-06 21:45:52 +0100

    Poprawiono działanie Kategorii, zmiany w migracjach

--- db/migrate/20170127191031_create_kategories.rb
+++ db/migrate/20170127191031_create_kategories.rb
@@ -2,7 +2,6 @@ class CreateKategories < ActiveRecord::Migration[5.0]
   def change
     create_table :kategories do |t|
       t.string "nazwa_kategorii"
-      t.integer "parent_id"
       t.timestamps
     end
   end
