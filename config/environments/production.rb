cb892ab1a25634beea7672285c0d4e66caabfd3b
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2017-02-09 19:16:24 +0100

    próba uruchomienia ckeditor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'ror-aukcja.herokuapp.com' }
+  config.assets.precompile += ['ckeditor/skins/kama/editor.css', 'ckeditor/lang/pl.js', 'ckeditor/skins/kama/editor.css']
 end
